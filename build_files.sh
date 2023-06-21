03d11c3e7d6bad30a860918c081e397e7b84f89a
maesterzak <abkdking1703@gmail.com>
2023-06-21 04:14:59 +0100

    fix python version error

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
