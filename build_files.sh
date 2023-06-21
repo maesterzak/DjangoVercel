b78e0fd52bb0ae355522097620c32cb2f23578b5
maesterzak <abkdking1703@gmail.com>
2023-06-21 04:13:47 +0100

    test database

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+python manage.py makemigrations
+python manage.py migrate
--- webapp/models.py
+++ webapp/models.py
@@ -6,4 +6,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    
+    summary = models.TextField(default="Summary of blog post")
